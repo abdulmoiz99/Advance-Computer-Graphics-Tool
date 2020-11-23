1314eeb5e8785d35f364342db55a4241414c4e2e
Abdul Moiz <moiza0799@gmail.com>
2020-11-23 13:00:35 +0500

    Dashboard

---
+++ Computer Grapics/frm_ImageCalculator.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Computer_Grapics
+{
+    partial class frm_ImageCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_ImageCalculator));
+            this.PanelFloat = new System.Windows.Forms.Panel();
+            this.Panel_SideBar = new System.Windows.Forms.Panel();
+            this.btn_Frm_Active = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Setting = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_systemInfo = new System.Windows.Forms.Button();
+            this.btn_DashBoard = new System.Windows.Forms.Button();
+            this.btn_Minimizesd = new System.Windows.Forms.Button();
+            this.xuiObjectEllipse1 = new XanderUI.XUIObjectEllipse();
+            this.Toppannel = new XanderUI.XUIGradientPanel();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.Panel_SideBar.SuspendLayout();
+            this.Toppannel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelFloat
+            // 
+            this.PanelFloat.BackColor = System.Drawing.Color.White;
+            this.PanelFloat.Location = new System.Drawing.Point(5, 188);
+            this.PanelFloat.Name = "PanelFloat";
+            this.PanelFloat.Size = new System.Drawing.Size(5, 49);
+            this.PanelFloat.TabIndex = 4;
+            // 
+            // Panel_SideBar
+            // 
+            this.Panel_SideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
+            this.Panel_SideBar.Controls.Add(this.btn_Frm_Active);
+            this.Panel_SideBar.Controls.Add(this.label1);
+            this.Panel_SideBar.Controls.Add(this.btn_Setting);
+            this.Panel_SideBar.Controls.Add(this.button1);
+            this.Panel_SideBar.Controls.Add(this.btn_systemInfo);
+            this.Panel_SideBar.Controls.Add(this.PanelFloat);
+            this.Panel_SideBar.Controls.Add(this.btn_DashBoard);
+            this.Panel_SideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel_SideBar.ForeColor = System.Drawing.Color.DarkCyan;
+            this.Panel_SideBar.Location = new System.Drawing.Point(0, 0);
+            this.Panel_SideBar.Name = "Panel_SideBar";
+            this.Panel_SideBar.Size = new System.Drawing.Size(336, 895);
+            this.Panel_SideBar.TabIndex = 10;
+            // 
+            // btn_Frm_Active
+            // 
+            this.btn_Frm_Active.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
+            this.btn_Frm_Active.FlatAppearance.BorderSize = 0;
+            this.btn_Frm_Active.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(60)))), ((int)(((byte)(114)))));
+            this.btn_Frm_Active.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Frm_Active.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Frm_Active.ForeColor = System.Drawing.Color.White;
+            this.btn_Frm_Active.Image = ((System.Drawing.Image)(resources.GetObject("btn_Frm_Active.Image")));
+            this.btn_Frm_Active.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Frm_Active.Location = new System.Drawing.Point(21, 260);
+            this.btn_Frm_Active.Name = "btn_Frm_Active";
+            this.btn_Frm_Active.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.btn_Frm_Active.Size = new System.Drawing.Size(312, 57);
+            this.btn_Frm_Active.TabIndex = 9;
+            this.btn_Frm_Active.Text = "Sprite Sheet";
+            this.btn_Frm_Active.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(96)))), ((int)(((byte)(133)))));
+            this.label1.Location = new System.Drawing.Point(41, 862);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Log Out";
+            // 
+            // btn_Setting
+            // 
+            this.btn_Setting.FlatAppearance.BorderSize = 0;
+            this.btn_Setting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Setting.Image = ((System.Drawing.Image)(resources.GetObject("btn_Setting.Image")));
+            this.btn_Setting.Location = new System.Drawing.Point(3, 855);
+            this.btn_Setting.Name = "btn_Setting";
+            this.btn_Setting.Size = new System.Drawing.Size(38, 40);
+            this.btn_Setting.TabIndex = 7;
+            this.btn_Setting.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(336, 172);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_systemInfo
+            // 
+            this.btn_systemInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
+            this.btn_systemInfo.FlatAppearance.BorderSize = 0;
+            this.btn_systemInfo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(60)))), ((int)(((byte)(114)))));
+            this.btn_systemInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_systemInfo.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_systemInfo.ForeColor = System.Drawing.Color.White;
+            this.btn_systemInfo.Image = ((System.Drawing.Image)(resources.GetObject("btn_systemInfo.Image")));
+            this.btn_systemInfo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_systemInfo.Location = new System.Drawing.Point(21, 332);
+            this.btn_systemInfo.Name = "btn_systemInfo";
+            this.btn_systemInfo.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.btn_systemInfo.Size = new System.Drawing.Size(312, 57);
+            this.btn_systemInfo.TabIndex = 6;
+            this.btn_systemInfo.Text = "Info";
+            this.btn_systemInfo.UseVisualStyleBackColor = false;
+            // 
+            // btn_DashBoard
+            // 
+            this.btn_DashBoard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(50)))), ((int)(((byte)(80)))));
+            this.btn_DashBoard.FlatAppearance.BorderSize = 0;
+            this.btn_DashBoard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(60)))), ((int)(((byte)(114)))));
+            this.btn_DashBoard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DashBoard.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DashBoard.ForeColor = System.Drawing.Color.White;
+            this.btn_DashBoard.Image = ((System.Drawing.Image)(resources.GetObject("btn_DashBoard.Image")));
+            this.btn_DashBoard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_DashBoard.Location = new System.Drawing.Point(21, 188);
+            this.btn_DashBoard.Name = "btn_DashBoard";
+            this.btn_DashBoard.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.btn_DashBoard.Size = new System.Drawing.Size(312, 57);
+            this.btn_DashBoard.TabIndex = 1;
+            this.btn_DashBoard.Text = "Manipulate";
+            this.btn_DashBoard.UseVisualStyleBackColor = false;
+            // 
+            // btn_Minimizesd
+            // 
+            this.btn_Minimizesd.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Minimizesd.FlatAppearance.BorderSize = 0;
+            this.btn_Minimizesd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Minimizesd.Image = ((System.Drawing.Image)(resources.GetObject("btn_Minimizesd.Image")));
+            this.btn_Minimizesd.Location = new System.Drawing.Point(715, 0);
+            this.btn_Minimizesd.Name = "btn_Minimizesd";
+            this.btn_Minimizesd.Size = new System.Drawing.Size(35, 37);
+            this.btn_Minimizesd.TabIndex = 11;
+            this.btn_Minimizesd.UseVisualStyleBackColor = false;
+            // 
+            // xuiObjectEllipse1
+            // 
+            this.xuiObjectEllipse1.CornerRadius = 20;
+            this.xuiObjectEllipse1.EffectedControl = this;
+            this.xuiObjectEllipse1.EffectedForm = this;
+            // 
+            // Toppannel
+            // 
+            this.Toppannel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Toppannel.BottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(212)))), ((int)(((byte)(104)))));
+            this.Toppannel.BottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(192)))), ((int)(((byte)(224)))));
+            this.Toppannel.Controls.Add(this.btn_close);
+            this.Toppannel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Toppannel.Location = new System.Drawing.Point(336, 0);
+            this.Toppannel.Name = "Toppannel";
+            this.Toppannel.PrimerColor = System.Drawing.Color.White;
+            this.Toppannel.Size = new System.Drawing.Size(1250, 37);
+            this.Toppannel.Style = XanderUI.XUIGradientPanel.GradientStyle.Corners;
+            this.Toppannel.TabIndex = 13;
+            this.Toppannel.TopLeft = System.Drawing.Color.DeepSkyBlue;
+            this.Toppannel.TopRight = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(192)))), ((int)(((byte)(224)))));
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_close.FlatAppearance.BorderSize = 0;
+            this.btn_close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Image = ((System.Drawing.Image)(resources.GetObject("btn_close.Image")));
+            this.btn_close.Location = new System.Drawing.Point(1191, 0);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(57, 35);
+            this.btn_close.TabIndex = 9;
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // frm_ImageCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1586, 895);
+            this.Controls.Add(this.Toppannel);
+            this.Controls.Add(this.Panel_SideBar);
+            this.Controls.Add(this.btn_Minimizesd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_ImageCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_ImageCalculator";
+            this.Panel_SideBar.ResumeLayout(false);
+            this.Panel_SideBar.PerformLayout();
+            this.Toppannel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelFloat;
+        private System.Windows.Forms.Panel Panel_SideBar;
+        private System.Windows.Forms.Button btn_Frm_Active;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Setting;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_systemInfo;
+        private System.Windows.Forms.Button btn_DashBoard;
+        private System.Windows.Forms.Button btn_Minimizesd;
+        private XanderUI.XUIObjectEllipse xuiObjectEllipse1;
+        private XanderUI.XUIGradientPanel Toppannel;
+        private System.Windows.Forms.Button btn_close;
+    }
+}
---
+++ Computer Grapics/frm_ImageManipulator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computer_Grapics
+{
+    public partial class frm_ImageManipulator : Form
+    {
+        public frm_ImageManipulator()
+        {
+            InitializeComponent();
+        }
+    }
+}
